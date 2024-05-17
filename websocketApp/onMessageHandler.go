0eeedb684ab8071db963df778fea4208d110e695
fabian thomas stuck <stuck.fabian@gmail.com>
2024-05-17 12:03:10 +0200

    refactoring

--- websocketApp/onMessageHandler.go
+++ websocketApp/onMessageHandler.go
@@ -13,7 +13,7 @@ func (app *App) OnMessageHandler(connection *Websocket.Connection, message *Mess
 		message.Origin = connection.Id
 		err := app.messageBrokerClient.Send(message)
 		if err != nil {
-			connection.SendMessage([]byte(Message.New("error", app.name, err.Error()).Serialize()))
+			connection.Send([]byte(Message.New("error", app.name, err.Error()).Serialize()))
 		}
 	}
 }
