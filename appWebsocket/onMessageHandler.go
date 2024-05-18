b40b3d46ec7b888630e399f8d169a0a77a36fdc8
fabian thomas stuck <stuck.fabian@gmail.com>
2024-05-18 13:15:47 +0200

    fixed security risk

--- appWebsocket/onMessageHandler.go
+++ appWebsocket/onMessageHandler.go
@@ -9,12 +9,14 @@ func (app *App) OnMessageHandler(connection *Websocket.Connection, message *Mess
 	switch message.Type {
 	case "heartbeat":
 		connection.ResetWatchdog()
-	default:
+	case "gridChange":
 		message.Origin = connection.Id
 		message.SyncKey = ""
 		err := app.messageBrokerClient.AsyncMessage(message)
 		if err != nil {
 			connection.Send([]byte(Message.New("error", "", app.name, err.Error()).Serialize()))
 		}
+	default:
+		connection.Send([]byte(Message.New("error", "", app.name, "Unknown message type").Serialize()))
 	}
 }
