72b06cd1a2ac6e6786e9d713a9d9bd385f56e6ef
fabian thomas stuck <stuck.fabian@gmail.com>
2024-05-25 19:01:35 +0200

    refactoring

--- main/main.go
+++ main/main.go
@@ -18,21 +18,28 @@ const TOPICRESOLUTIONSERVER_ADDRESS = ":60002"
 const HTTP_DEV_PORT = ":8080"
 const WEBSOCKET_PORT = ":8443"
 
+const GET_GRID_SYNC_TOPIC = "getGridSync"
+const GRID_CHANGE_TOPIC = "gridChange"
+const NEXT_GENERATION_TOPIC = "nextGeneration"
+const SET_GRID_TOPIC = "setGrid"
+const GET_GRID_TOPIC = "getGrid"
+const GET_GRID_CHANGE_TOPIC = "getGridChange"
+
 func main() {
 	logger := Utilities.NewLogger("error_log.txt")
 
-	messageBrokerServerA := MessageBrokerServer.New("messageBrokerServer", MESSAGEBROKERSERVER_A_ADDRESS, logger)
+	messageBrokerServerA := MessageBrokerServer.New("messageBrokerServerA", MESSAGEBROKERSERVER_A_ADDRESS, logger)
 	messageBrokerServerA.Start()
-	messageBrokerServerA.AddTopics("getGridSync", "gridChange", "nextGeneration", "setGrid")
+	messageBrokerServerA.AddTopics(GET_GRID_SYNC_TOPIC, GRID_CHANGE_TOPIC, NEXT_GENERATION_TOPIC, SET_GRID_TOPIC)
 
-	messageBrokerServerB := MessageBrokerServer.New("messageBrokerServer", MESSAGEBROKERSERVER_B_ADDRESS, logger)
+	messageBrokerServerB := MessageBrokerServer.New("messageBrokerServerB", MESSAGEBROKERSERVER_B_ADDRESS, logger)
 	messageBrokerServerB.Start()
-	messageBrokerServerB.AddTopics("getGrid", "getGridChange")
+	messageBrokerServerB.AddTopics(GET_GRID_TOPIC, GET_GRID_CHANGE_TOPIC)
 
 	topicResolutionServer := TopicResolutionServer.New("topicResolutionServer", TOPICRESOLUTIONSERVER_ADDRESS, logger)
 	topicResolutionServer.Start()
-	topicResolutionServer.RegisterTopics(MESSAGEBROKERSERVER_A_ADDRESS, "getGridSync", "gridChange", "nextGeneration", "setGrid")
-	topicResolutionServer.RegisterTopics(MESSAGEBROKERSERVER_B_ADDRESS, "getGrid", "getGridChange")
+	topicResolutionServer.RegisterTopics(MESSAGEBROKERSERVER_A_ADDRESS, GET_GRID_SYNC_TOPIC, GRID_CHANGE_TOPIC, NEXT_GENERATION_TOPIC, SET_GRID_TOPIC)
+	topicResolutionServer.RegisterTopics(MESSAGEBROKERSERVER_B_ADDRESS, GET_GRID_TOPIC, GET_GRID_CHANGE_TOPIC)
 
 	messageBrokerClientWebsocket := MessageBrokerClient.New("messageBrokerClientWebsocket", TOPICRESOLUTIONSERVER_ADDRESS, logger)
 	messageBrokerClientWebsocket.Connect()
@@ -46,13 +53,13 @@ func main() {
 	appWebsocket := appWebsocket.New("websocketApp", logger, messageBrokerClientWebsocket, websocketServer)
 	appGameOfLife := appGameOfLife.New("gameOfLifeApp", logger, messageBrokerClientGameOfLife, 90, 140)
 
-	messageBrokerClientGameOfLife.RegisterIncomingSyncTopic("getGridSync", appGameOfLife.GetGridSync)
-	messageBrokerClientGameOfLife.RegisterIncomingAsyncTopic("gridChange", appGameOfLife.GridChange)
-	messageBrokerClientGameOfLife.RegisterIncomingAsyncTopic("nextGeneration", appGameOfLife.NextGeneration)
-	messageBrokerClientGameOfLife.RegisterIncomingAsyncTopic("setGrid", appGameOfLife.SetGrid)
+	messageBrokerClientGameOfLife.RegisterIncomingSyncTopic(GET_GRID_SYNC_TOPIC, appGameOfLife.GetGridSync)
+	messageBrokerClientGameOfLife.RegisterIncomingAsyncTopic(GRID_CHANGE_TOPIC, appGameOfLife.GridChange)
+	messageBrokerClientGameOfLife.RegisterIncomingAsyncTopic(NEXT_GENERATION_TOPIC, appGameOfLife.NextGeneration)
+	messageBrokerClientGameOfLife.RegisterIncomingAsyncTopic(SET_GRID_TOPIC, appGameOfLife.SetGrid)
 
-	messageBrokerClientWebsocket.RegisterIncomingAsyncTopic("getGrid", appWebsocket.GetGrid)
-	messageBrokerClientWebsocket.RegisterIncomingAsyncTopic("getGridChange", appWebsocket.GetGridChange)
+	messageBrokerClientWebsocket.RegisterIncomingAsyncTopic(GET_GRID_TOPIC, appWebsocket.GetGrid)
+	messageBrokerClientWebsocket.RegisterIncomingAsyncTopic(GET_GRID_CHANGE_TOPIC, appWebsocket.GetGridChange)
 
 	websocketServer.SetOnMessageHandler(appWebsocket.OnMessageHandler)
 	websocketServer.SetOnConnectHandler(appWebsocket.OnConnectHandler)
