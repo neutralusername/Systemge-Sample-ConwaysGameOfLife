57153d4703489e2135604a8c4213c08dc9fb5071
fabian thomas stuck <stuck.fabian@gmail.com>
2024-05-30 19:34:51 +0200

    refactoring

--- main/main.go
+++ main/main.go
@@ -18,17 +18,17 @@ const WEBSOCKET_PORT = ":8443"
 const ERROR_LOG_FILE = "error_log.txt"
 
 func main() {
-	messageBrokerServerA := Module.NewMessageBrokerServerModule("messageBrokerServerA", MESSAGEBROKERSERVER_A_ADDRESS, ERROR_LOG_FILE,
+	messageBrokerServerA := Module.NewMessageBrokerServer("messageBrokerServerA", MESSAGEBROKERSERVER_A_ADDRESS, ERROR_LOG_FILE,
 		topics.GET_GRID_SYNC,
 		topics.GRID_CHANGE,
 		topics.NEXT_GENERATION,
 		topics.SET_GRID,
 	)
-	messageBrokerServerB := Module.NewMessageBrokerServerModule("messageBrokerServerB", MESSAGEBROKERSERVER_B_ADDRESS, ERROR_LOG_FILE,
+	messageBrokerServerB := Module.NewMessageBrokerServer("messageBrokerServerB", MESSAGEBROKERSERVER_B_ADDRESS, ERROR_LOG_FILE,
 		topics.GET_GRID,
 		topics.GET_GRID_CHANGE,
 	)
-	topicResolutionServer := Module.NewTopicResolutionModule("topicResolutionServer", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE, map[string]string{
+	topicResolutionServer := Module.NewTopicResolution("topicResolutionServer", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE, map[string]string{
 		topics.GET_GRID_SYNC:   MESSAGEBROKERSERVER_A_ADDRESS,
 		topics.GRID_CHANGE:     MESSAGEBROKERSERVER_A_ADDRESS,
 		topics.NEXT_GENERATION: MESSAGEBROKERSERVER_A_ADDRESS,
@@ -39,8 +39,8 @@ func main() {
 	httpServe := Module.NewHTTPModule("HTTPfrontend", HTTP_DEV_PORT, "", "", map[string]func(w http.ResponseWriter, r *http.Request){
 		"/": HTTP.SendDirectory("../frontend"),
 	})
-	messageBrokerClientGameOfLife := Module.NewMessageBrokerClientModule("messageBrokerClientGameOfLife", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE, appGameOfLife.New)
-	messageBrokerClientWebsocket := Module.NewWebsocketClientModule("messageBrokerClientWebsocket", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE, "/ws", WEBSOCKET_PORT, "", "", appWebsocket.New)
+	messageBrokerClientGameOfLife := Module.NewMessageBrokerClient("messageBrokerClientGameOfLife", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE, appGameOfLife.New)
+	messageBrokerClientWebsocket := Module.NewWebsocketClient("messageBrokerClientWebsocket", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE, "/ws", WEBSOCKET_PORT, "", "", appWebsocket.New)
 
 	Module.CommandLoop(Module.NewMultiModule(
 		messageBrokerServerA,
