3deb6ff2c9044b54f24e36189b911bf13405c221
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-01 13:42:10 +0200

    refactoring

--- appWebsocket/app.go
+++ appWebsocket/app.go
@@ -1,6 +1,8 @@
 package appWebsocket
 
 import (
+	"Systemge/Error"
+	"Systemge/Message"
 	"Systemge/MessageBrokerClient"
 	"Systemge/Utilities"
 	"SystemgeSampleApp/topic"
@@ -24,6 +26,10 @@ func (app *App) GetAsyncMessageHandlers() map[string]MessageBrokerClient.AsyncMe
 		topic.GET_GRID_CHANGE: app.WebsocketPropagate,
 	}
 }
+func (app *App) WebsocketPropagate(message *Message.Message) error {
+	app.messageBrokerClient.WebsocketBroadcast([]byte(message.Serialize()))
+	return nil
+}
 
 func (app *App) GetSyncMessageHandlers() map[string]MessageBrokerClient.SyncMessageHandler {
 	return map[string]MessageBrokerClient.SyncMessageHandler{}
@@ -31,4 +37,32 @@ func (app *App) GetSyncMessageHandlers() map[string]MessageBrokerClient.SyncMess
 
 func (app *App) GetCustomCommandHandlers() map[string]func([]string) error {
 	return map[string]func([]string) error{}
+}
+
+func (app *App) GetWebsocketMessageHandlers() map[string]MessageBrokerClient.WebsocketMessageHandler {
+	return map[string]MessageBrokerClient.WebsocketMessageHandler{
+		topic.GRID_CHANGE:     app.PropagateWebsocketAsyncMessage,
+		topic.NEXT_GENERATION: app.PropagateWebsocketAsyncMessage,
+		topic.SET_GRID:        app.PropagateWebsocketAsyncMessage,
+	}
+}
+func (app *App) PropagateWebsocketAsyncMessage(connection *MessageBrokerClient.WebsocketClient, message *Message.Message) error {
+	return app.messageBrokerClient.AsyncMessage(message)
+}
+
+func (app *App) GetOnConnectHandler() MessageBrokerClient.OnConnectHandler {
+	return func(connection *MessageBrokerClient.WebsocketClient) {
+		response, err := app.messageBrokerClient.SyncMessage(Message.NewSync(topic.GET_GRID_SYNC, app.messageBrokerClient.GetName(), connection.Id))
+		if err != nil {
+			app.logger.Log(Error.New(err.Error()).Error())
+			return
+		}
+		connection.Send([]byte(response.Serialize()))
+	}
+}
+
+func (app *App) GetOnDisconnectHandler() MessageBrokerClient.OnDisconnectHandler {
+	return func(connection *MessageBrokerClient.WebsocketClient) {
+		app.logger.Log("Connection closed")
+	}
 }
