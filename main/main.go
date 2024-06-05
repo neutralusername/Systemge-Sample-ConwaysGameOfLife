e609d316f0eeb154cfbcd769fa0045599c54423a
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-05 15:34:32 +0200

    refactoring

--- main/main.go
+++ main/main.go
@@ -1,47 +1,24 @@
 package main
 
 import (
-	"Systemge/HTTP"
 	"Systemge/Module"
-	"Systemge/Topics"
 	"SystemgeSampleApp/appGameOfLife"
 	"SystemgeSampleApp/appWebsocket"
-	"SystemgeSampleApp/topic"
 )
 
-const MESSAGEBROKERSERVER_A_ADDRESS = ":60003"
-const MESSAGEBROKERSERVER_B_ADDRESS = ":60004"
-const TOPICRESOLUTIONSERVER_ADDRESS = ":60002"
+const TOPICRESOLUTIONSERVER_ADDRESS = ":60000"
 const HTTP_DEV_PORT = ":8080"
 const WEBSOCKET_PORT = ":8443"
 
 const ERROR_LOG_FILE_PATH = "error.log"
 
-var topics = Topics.Registry{
-	topic.GET_GRID_SYNC:   MESSAGEBROKERSERVER_A_ADDRESS,
-	topic.GRID_CHANGE:     MESSAGEBROKERSERVER_A_ADDRESS,
-	topic.NEXT_GENERATION: MESSAGEBROKERSERVER_A_ADDRESS,
-	topic.SET_GRID:        MESSAGEBROKERSERVER_A_ADDRESS,
-	topic.GET_GRID:        MESSAGEBROKERSERVER_B_ADDRESS,
-	topic.GET_GRID_CHANGE: MESSAGEBROKERSERVER_B_ADDRESS,
-}
-var servePatterns = HTTP.PatternRegistry{
-	"/": HTTP.SendDirectory("../frontend"),
-}
-
 func main() {
-	httpServe := Module.NewHTTPServer("HTTPfrontend", HTTP_DEV_PORT, "", "", servePatterns)
-	topicResolutionServer := Module.NewTopicResolutionServer("topicResolutionServer", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE_PATH, topics)
-	messageBrokerServerA := Module.NewMessageBrokerServer("messageBrokerServerA", MESSAGEBROKERSERVER_A_ADDRESS, ERROR_LOG_FILE_PATH, topics.GetTopics(MESSAGEBROKERSERVER_A_ADDRESS)...)
-	messageBrokerServerB := Module.NewMessageBrokerServer("messageBrokerServerB", MESSAGEBROKERSERVER_B_ADDRESS, ERROR_LOG_FILE_PATH, topics.GetTopics(MESSAGEBROKERSERVER_B_ADDRESS)...)
-	messageBrokerClientGameOfLife := Module.NewMessageBrokerClient("messageBrokerClientGameOfLife", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE_PATH, appGameOfLife.New)
-	messageBrokerClientWebsocket := Module.NewWebsocketClient("messageBrokerClientWebsocket", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE_PATH, "/ws", WEBSOCKET_PORT, "", "", appWebsocket.New)
-	Module.CommandLoop(Module.NewMultiModule(
-		topicResolutionServer,
-		messageBrokerServerA,
-		messageBrokerServerB,
-		messageBrokerClientGameOfLife,
-		messageBrokerClientWebsocket,
-		httpServe,
-	), messageBrokerClientGameOfLife.GetApplication().GetCustomCommandHandlers())
+	Module.StartSystemgeConsole(Module.NewMultiModule(
+		Module.NewResolverServerFromConfig("topicResolutionServer.systemge", ERROR_LOG_FILE_PATH),
+		Module.NewBrokerServerFromConfig("messageBrokerServerA.systemge", ERROR_LOG_FILE_PATH),
+		Module.NewBrokerServerFromConfig("messageBrokerServerB.systemge", ERROR_LOG_FILE_PATH),
+		Module.NewClient("clientGameOfLife", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE_PATH, appGameOfLife.New),
+		Module.NewWebsocketClient("clientWebsocket", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE_PATH, "/ws", WEBSOCKET_PORT, "", "", appWebsocket.New),
+		Module.NewHTTPServerFromConfig("httpServe.systemge", ERROR_LOG_FILE_PATH),
+	), nil)
 }
