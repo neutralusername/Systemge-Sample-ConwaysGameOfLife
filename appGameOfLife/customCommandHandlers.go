81c819267eae6f92a12d4c7390cf2e4989d6fc00
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-09 10:02:39 +0200

    added "chess" command to generate a checker-pattern

--- appGameOfLife/customCommandHandlers.go
+++ appGameOfLife/customCommandHandlers.go
@@ -11,6 +11,7 @@ func (app *App) GetCustomCommandHandlers() map[string]Application.CustomCommandH
 	return map[string]Application.CustomCommandHandler{
 		"randomize": app.randomizeGrid,
 		"invert":    app.invertGrid,
+		"chess":     app.chessGrid,
 	}
 }
 
@@ -35,6 +36,21 @@ func (app *App) invertGrid(args []string) error {
 	for row := 0; row < app.gridRows; row++ {
 		for col := 0; col < app.gridCols; col++ {
 			app.grid[row][col] = 1 - app.grid[row][col]
+		}
+	}
+	err := app.messageBrokerClient.AsyncMessage(Message.NewAsync(topic.GET_GRID, app.messageBrokerClient.GetName(), dto.NewGrid(app.grid, app.gridRows, app.gridCols).Marshal()))
+	if err != nil {
+		app.logger.Log(err.Error())
+	}
+	return nil
+}
+
+func (app *App) chessGrid(args []string) error {
+	app.mutex.Lock()
+	defer app.mutex.Unlock()
+	for row := 0; row < app.gridRows; row++ {
+		for col := 0; col < app.gridCols; col++ {
+			app.grid[row][col] = (row + col) % 2
 		}
 	}
 	err := app.messageBrokerClient.AsyncMessage(Message.NewAsync(topic.GET_GRID, app.messageBrokerClient.GetName(), dto.NewGrid(app.grid, app.gridRows, app.gridCols).Marshal()))
