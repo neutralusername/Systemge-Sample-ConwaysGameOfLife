2079a4bf54d8f27497a864be74b4588a39579e74
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-09 08:07:57 +0200

    added gameOfLife custom commands to cli

--- main/main.go
+++ main/main.go
@@ -12,12 +12,13 @@ const WEBSOCKET_PORT = ":8443"
 const ERROR_LOG_FILE_PATH = "error.log"
 
 func main() {
+	clientGameOfLife := Module.NewClient("clientGameOfLife", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE_PATH, appGameOfLife.New)
 	Module.StartCommandLineInterface(Module.NewMultiModule(
 		Module.NewResolverServerFromConfig("resolver.systemge", ERROR_LOG_FILE_PATH),
 		Module.NewBrokerServerFromConfig("brokerGameOfLife.systemge", ERROR_LOG_FILE_PATH),
 		Module.NewBrokerServerFromConfig("brokerWebsocket.systemge", ERROR_LOG_FILE_PATH),
-		Module.NewClient("clientGameOfLife", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE_PATH, appGameOfLife.New),
+		clientGameOfLife,
 		Module.NewWebsocketClient("clientWebsocket", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE_PATH, "/ws", WEBSOCKET_PORT, "", "", appWebsocket.New),
 		Module.NewHTTPServerFromConfig("httpServe.systemge", ERROR_LOG_FILE_PATH),
-	), nil)
+	), clientGameOfLife.GetApplication().GetCustomCommandHandlers())
 }
