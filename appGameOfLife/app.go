d21558b3d8bd8e0dbfd071c56b4d9ce3abd46c40
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-13 10:56:11 +0200

    bugfix (introduced by moving functionality to onStart())

--- appGameOfLife/app.go
+++ appGameOfLife/app.go
@@ -32,9 +32,9 @@ func New(logger *Utilities.Logger, messageBrokerClient *MessageBrokerClient.Clie
 }
 
 func (app *App) OnStart() error {
-	grid := make([][]int, app.gridCols)
+	grid := make([][]int, app.gridRows)
 	for i := range grid {
-		grid[i] = make([]int, app.gridRows)
+		grid[i] = make([]int, app.gridCols)
 	}
 	app.grid = grid
 	return nil
