d309008089fb56f83d65b74860d112b96663b46d
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-15 12:58:54 +0200

    refactoring

--- main/main.go
+++ main/main.go
@@ -14,9 +14,9 @@ const ERROR_LOG_FILE_PATH = "error.log"
 func main() {
 	clientGameOfLife := Module.NewClient("clientGameOfLife", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE_PATH, appGameOfLife.New, nil)
 	Module.StartCommandLineInterface(Module.NewMultiModule(
-		Module.NewResolverServerFromConfig("resolver.systemge", ERROR_LOG_FILE_PATH),
-		Module.NewBrokerServerFromConfig("brokerGameOfLife.systemge", ERROR_LOG_FILE_PATH),
-		Module.NewBrokerServerFromConfig("brokerWebsocket.systemge", ERROR_LOG_FILE_PATH),
+		Module.NewResolverFromConfig("resolver.systemge", ERROR_LOG_FILE_PATH),
+		Module.NewBrokerFromConfig("brokerGameOfLife.systemge", ERROR_LOG_FILE_PATH),
+		Module.NewBrokerFromConfig("brokerWebsocket.systemge", ERROR_LOG_FILE_PATH),
 		clientGameOfLife,
 		Module.NewWebsocketClient("clientWebsocket", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE_PATH, "/ws", WEBSOCKET_PORT, "", "", appWebsocket.New, nil),
 		Module.NewHTTPServerFromConfig("httpServe.systemge", ERROR_LOG_FILE_PATH),
