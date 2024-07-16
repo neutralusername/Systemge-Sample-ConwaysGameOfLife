abed0add34d0137c29b7ca162c45a51713dc9762
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-16 18:42:01 +0200

    removed unused code

--- main/main.go
+++ main/main.go
@@ -10,14 +10,6 @@ import (
 	"SystemgeSampleConwaysGameOfLife/appWebsocketHTTP"
 )
 
-const RESOLVER_ADDRESS = "127.0.0.1:60000"
-const RESOLVER_NAME_INDICATION = "127.0.0.1"
-const RESOLVER_TLS_CERT_PATH = "MyCertificate.crt"
-const WEBSOCKET_PORT = ":8443"
-const HTTP_PORT = ":8080"
-
-const ERROR_LOG_FILE_PATH = "error.log"
-
 func main() {
 	err := Resolver.New(Config.ParseResolverConfigFromFile("resolver.systemge")).Start()
 	if err != nil {
