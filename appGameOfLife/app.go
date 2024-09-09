d7805f43c515c5ce6eba0aacc8c5188fd40e4591
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-09 19:12:34 +0200

    activated reconnect in sample

--- appGameOfLife/app.go
+++ appGameOfLife/app.go
@@ -56,6 +56,7 @@ func New() *App {
 					Address: "localhost:60001",
 				},
 			},
+			Reconnect:        true,
 			ConnectionConfig: &Config.TcpSystemgeConnection{},
 		},
 		func(connection SystemgeConnection.SystemgeConnection) error {
