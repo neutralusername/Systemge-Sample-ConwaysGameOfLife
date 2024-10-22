1401db8ea9647f8afa1746acc44042e16618c5f6
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-22 16:59:58 +0200

    wip

--- appGameOfLife/app.go
+++ appGameOfLife/app.go
@@ -71,45 +71,6 @@ func New() *App {
 	return app
 }
 
-func (app *App) calcNextGeneration() {
-	app.mutex.Lock()
-	defer app.mutex.Unlock()
-
-	nextGrid := make([][]int, app.gridRows)
-	for i := range nextGrid {
-		nextGrid[i] = make([]int, app.gridCols)
-	}
-	for row := 0; row < app.gridRows; row++ {
-		for col := 0; col < app.gridCols; col++ {
-			aliveNeighbours := 0
-			for i := -1; i < 2; i++ {
-				for j := -1; j < 2; j++ {
-					if app.toroidal {
-						neighbourRow := (row + i + app.gridRows) % app.gridRows
-						neighbourCol := (col + j + app.gridCols) % app.gridCols
-						aliveNeighbours += app.grid[neighbourRow][neighbourCol]
-					} else {
-						neighbourRow := row + i
-						neighbourCol := col + j
-						if neighbourRow >= 0 && neighbourRow < app.gridRows && neighbourCol >= 0 && neighbourCol < app.gridCols {
-							aliveNeighbours += app.grid[neighbourRow][neighbourCol]
-						}
-					}
-				}
-			}
-			aliveNeighbours -= app.grid[row][col]
-			if app.grid[row][col] == 1 && (aliveNeighbours < 2 || aliveNeighbours > 3) {
-				nextGrid[row][col] = 0
-			} else if app.grid[row][col] == 0 && aliveNeighbours == 3 {
-				nextGrid[row][col] = 1
-			} else {
-				nextGrid[row][col] = app.grid[row][col]
-			}
-		}
-	}
-	app.grid = nextGrid
-}
-
 func (app *App) acceptHandler(connection systemge.Connection[*tools.Message]) error {
 	reader, err := serviceReader.NewAsync(
 		connection,
@@ -197,6 +158,45 @@ func (app *App) readHandler(message *tools.Message, connection systemge.Connecti
 			panic(err)
 		}
 	}
+}
+
+func (app *App) calcNextGeneration() {
+	app.mutex.Lock()
+	defer app.mutex.Unlock()
+
+	nextGrid := make([][]int, app.gridRows)
+	for i := range nextGrid {
+		nextGrid[i] = make([]int, app.gridCols)
+	}
+	for row := 0; row < app.gridRows; row++ {
+		for col := 0; col < app.gridCols; col++ {
+			aliveNeighbours := 0
+			for i := -1; i < 2; i++ {
+				for j := -1; j < 2; j++ {
+					if app.toroidal {
+						neighbourRow := (row + i + app.gridRows) % app.gridRows
+						neighbourCol := (col + j + app.gridCols) % app.gridCols
+						aliveNeighbours += app.grid[neighbourRow][neighbourCol]
+					} else {
+						neighbourRow := row + i
+						neighbourCol := col + j
+						if neighbourRow >= 0 && neighbourRow < app.gridRows && neighbourCol >= 0 && neighbourCol < app.gridCols {
+							aliveNeighbours += app.grid[neighbourRow][neighbourCol]
+						}
+					}
+				}
+			}
+			aliveNeighbours -= app.grid[row][col]
+			if app.grid[row][col] == 1 && (aliveNeighbours < 2 || aliveNeighbours > 3) {
+				nextGrid[row][col] = 0
+			} else if app.grid[row][col] == 0 && aliveNeighbours == 3 {
+				nextGrid[row][col] = 1
+			} else {
+				nextGrid[row][col] = app.grid[row][col]
+			}
+		}
+	}
+	app.grid = nextGrid
 }
 
 /*
